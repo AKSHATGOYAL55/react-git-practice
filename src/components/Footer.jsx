7f33051fe58e49d7027837cc23e4fa9aff949c9a
Akshat Goyal <akshatg525@gmail.com>
2026-01-21 11:56:53 +0530

    Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-green-400'>
+      Footer
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='bg-red-200 h-10 p-2 m-5 border-2 border-blue-500'>
-      Navbar is looking crazy! wow
+      Navbar 
     </div>
+    
   )
 }
 
